a57e65ef233d041d911f75b4b9bd762169386716
yxy <1140783177@qq.com>
2024-02-29 23:33:27 +0000

    automated test- instrumentation test and assertExists

--- app/src/androidTest/java/com/example/first/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/first/ExampleInstrumentedTest.kt
@@ -1,12 +1,16 @@
 package com.example.first
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.first.ui.theme.FirstTheme
+import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import java.text.NumberFormat
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +24,22 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.first", appContext.packageName)
+    }
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            FirstTheme {
+                TipTimeLayout()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount")
+            .performTextInput("10")
+        composeTestRule.onNodeWithText("Tip Percentage")
+            .performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
+            "No node with this text was found.")
     }
 }
