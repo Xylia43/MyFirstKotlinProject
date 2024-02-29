4253abd69917c25949a74b1867ee276d37a7b451
yxy <1140783177@qq.com>
2024-02-29 23:09:21 +0000

    learn to write automated test,assertions

--- app/src/test/java/com/example/first/ExampleUnitTest.kt
+++ app/src/test/java/com/example/first/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.example.first
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
+import java.text.NumberFormat
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,13 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+    @Test
+    fun calculateTip_20PercentNoRoundup() {
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+        assertEquals(expectedTip, actualTip)
     }
 }
