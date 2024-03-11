f1f5973eeab5169c59c87eb17f7c96f6990852f6
yxy <1140783177@qq.com>
2024-03-11 23:37:35 +0000

    Unit3,pathway 3,add expanded item spring animation and color change

--- app/src/main/java/com/example/first/MainActivity.kt
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -7,6 +7,10 @@ import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.annotation.VisibleForTesting
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -1089,8 +1093,14 @@ fun DogItem(
     modifier: Modifier = Modifier
 ) {
     var expanded by remember { mutableStateOf(false) }
+    val color by animateColorAsState(targetValue = if (expanded) MaterialTheme.colorScheme.tertiaryContainer
+    else MaterialTheme.colorScheme.primaryContainer,)
     Card(modifier = modifier) {
-        Column() {
+        Column(modifier = Modifier
+            .animateContentSize(animationSpec = spring(
+                dampingRatio = Spring.DampingRatioNoBouncy,
+                stiffness = Spring.StiffnessMedium))
+            .background(color = color)) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
