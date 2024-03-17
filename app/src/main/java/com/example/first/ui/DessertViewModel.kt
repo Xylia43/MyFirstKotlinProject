80efd4dba62144ff27c8ad981ca2f5fe2725b5cf
yxy <1140783177@qq.com>
2024-03-17 22:59:04 +0000

    Unit4,pathway 1, official solution of viewmodel for dessert app

--- app/src/main/java/com/example/first/ui/DessertViewModel.kt
+++ app/src/main/java/com/example/first/ui/DessertViewModel.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import com.example.first.R
+import com.example.first.data.DessertDatasource.dessertList
 import com.example.first.data.DessertUiState
 import com.example.first.model.Dessert
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -20,14 +21,39 @@ class DessertViewModel : ViewModel() {
 
     fun onDessertClicked(){
         _uiState.update { currentState ->
+            val dessertsSold = currentState.dessertsSold + 1
+            val nextDessertIndex = determineDessertIndex(dessertsSold)
+//            val nextDessertIndex = determineDessertToShow(dessertList,dessertsSold)
             currentState.copy(
+                currentDessertIndex = nextDessertIndex,
                 revenue = currentState.revenue + currentState.currentDessertPrice,
-                dessertsSold=currentState.dessertsSold + 1,
-                currentDessertIndex = currentState.currentDessertIndex + 1,
-                currentDessertImageId = currentState.currentDessertImageId,
-                currentDessertPrice = currentState.currentDessertPrice
+                dessertsSold = dessertsSold,
+                currentDessertImageId = dessertList[nextDessertIndex].imageId,
+                currentDessertPrice = dessertList[nextDessertIndex].price
             )
+//            currentState.copy(
+//                revenue = currentState.revenue + currentState.currentDessertPrice,
+//                dessertsSold=currentState.dessertsSold + 1,
+//                currentDessertIndex = currentState.currentDessertIndex + 1,
+//                currentDessertImageId = currentState.currentDessertImageId,
+//                currentDessertPrice = currentState.currentDessertPrice
+//            )
         }
+    }
+    private fun determineDessertIndex(dessertsSold: Int): Int {
+        var dessertIndex = 0
+        for (index in dessertList.indices) {
+            if (dessertsSold >= dessertList[index].startProductionAmount) {
+                dessertIndex = index
+            } else {
+                // The list of desserts is sorted by startProductionAmount. As you sell more
+                // desserts, you'll start producing more expensive desserts as determined by
+                // startProductionAmount. We know to break as soon as we see a dessert who's
+                // "startProductionAmount" is greater than the amount sold.
+                break
+            }
+        }
+        return dessertIndex
     }
     /**
      * Determine which dessert to show.
