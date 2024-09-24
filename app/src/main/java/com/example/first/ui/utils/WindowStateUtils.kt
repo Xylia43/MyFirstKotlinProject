b2fec9614864aef7b2b126ca90a1e003c61b0662
yxy <1140783177@qq.com>
2024-09-24 18:29:27 +0800

    Unit 4,pathway 3, adaptive app with dynamic navigation

---
+++ app/src/main/java/com/example/first/ui/utils/WindowStateUtils.kt
@@ -0,0 +1,14 @@
+package com.example.first.ui.utils
+
+/**
+ * @className: first
+ * @desc:
+ * @version:
+ * @author: blossom
+ * @email:
+ * @date: 2024/9/24 18:03 Tue
+ * @copyright: Copyright (c) 2024 Yxy Inc. All Rights Reserved.
+ */
+enum class ReplyNavigationType {
+    BOTTOM_NAVIGATION, NAVIGATION_RAIL, PERMANENT_NAVIGATION_DRAWER
+}
