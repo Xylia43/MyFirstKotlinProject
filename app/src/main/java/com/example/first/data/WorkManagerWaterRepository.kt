4faf463c872fb7de90967f3a1abb2234e5b6df47
yxy <1140783177@qq.com>
2024-10-18 17:35:48 +0800

    Unit 7,pathway 1, workmanager -water me complete

--- app/src/main/java/com/example/first/data/WorkManagerWaterRepository.kt
+++ app/src/main/java/com/example/first/data/WorkManagerWaterRepository.kt
@@ -10,8 +10,12 @@ package com.example.first.data
  * @copyright: Copyright (c) 2024 Yxy Inc. All Rights Reserved.
  */
 import android.content.Context
+import androidx.work.Data
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.first.model.Plant
+import com.example.first.workers.WaterReminderWorker
 import java.util.concurrent.TimeUnit
 
 class WorkManagerWaterRepository(context: Context) : WaterRepository {
@@ -20,5 +24,27 @@ class WorkManagerWaterRepository(context: Context) : WaterRepository {
     override val plants: List<Plant>
         get() = DataSource.plants
 
-    override fun scheduleReminder(duration: Long, unit: TimeUnit, plantName: String) {}
+    override fun scheduleReminder(duration: Long, unit: TimeUnit, plantName: String) {
+//        var data = workManager.beginUniqueWork(//Blur-O-Matic now only blurs one image at a time.
+//            WaterReminderWorker.nameKey ,
+//            ExistingWorkPolicy.REPLACE,
+//            OneTimeWorkRequest.from(WaterReminderWorker::class.java))
+//        val watermeBuilder = OneTimeWorkRequestBuilder<WaterReminderWorker>()
+//        watermeBuilder.setInputData(data)
+
+        val data = Data.Builder()
+        data.putString(WaterReminderWorker.nameKey, plantName)
+
+        val workRequestBuilder = OneTimeWorkRequestBuilder<WaterReminderWorker>()
+            .setInitialDelay(duration, unit)
+            .setInputData(data.build())
+            .build()
+
+        workManager.enqueueUniqueWork(
+            plantName + duration,
+            ExistingWorkPolicy.REPLACE,
+            workRequestBuilder
+        )
+
+    }
 }
